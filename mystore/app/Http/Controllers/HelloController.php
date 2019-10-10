c99da3b84aaea5191723789441e0715c1441d50a
Lei Hua <leihua2009@gmail.com>
2019-10-09 20:35:59 -0400

    pass variables

--- mystore/app/Http/Controllers/HelloController.php
+++ mystore/app/Http/Controllers/HelloController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, $name)
     {
-        return view('hello');
+        $data = [
+            'name' => $name
+        ];
+        return view('hello', $data);
     }
 }
--- mystore/routes/web.php
+++ mystore/routes/web.php
@@ -4,4 +4,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', 'HelloController@index');
+Route::get('/hello/{name}', 'HelloController@index');
