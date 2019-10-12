cef5118db8d7fb2ecf5b1a96098fa2de239a6438
Lei Hua <leihua2009@gmail.com>
2019-10-11 21:08:16 -0400

    console routes

--- mystore/routes/console.php
+++ mystore/routes/console.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('monty', function () {
+    $this->info('Spam eggs sausage spam sausage eggs');
+})->describe('Display an important message from monty python');
