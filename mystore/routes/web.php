09575f6e2956f23e3a68fcd788dbe650194e1015
Lei Hua <leihua2009@gmail.com>
2019-10-12 21:09:23 -0400

    routing and parameters

--- mystore/routes/web.php
+++ mystore/routes/web.php
@@ -2,10 +2,22 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/hello/{name}', 'HelloController@index');
+Route::get('/hello/{name}', 'HelloController@index')->name('hello');
 
 Route::get('/about-us', function () {
     return 'About us page';
-});
+})->name('about.us');
+
+Route::get('/pages/{name}', function ($name) {
+    return "$name page";
+})->name('pages');
+
+Route::get('/products/{url}', function ($url) {
+    return $url . "page details";
+})->name('products');
+
+Route::get('/categories/{url?}', function ($url = null) {
+    return $url == null ? 'list all categories' : "display $url category";
+})->name('categories');
